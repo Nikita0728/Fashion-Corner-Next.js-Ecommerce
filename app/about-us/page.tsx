39633e2bd0c8a83c664937382a83d1025bab4809
Nikita0728 <kunwornikita123@gmail.com>
2024-11-16 00:05:53 +0545

    fix: about us page

---
+++ app/about-us/page.tsx
@@ -0,0 +1,14 @@
+import TeamSection from '@/components/team/team'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h2 className='text-2xl font-bold flex justify-center mt-5'>About Our Shop</h2>
+        <p className='text-justify mx-[5rem] mt-5'>Zenna Shop is a very slick and clean e-commerce template with endless possibilities. Creating an awesome clothes store with this Theme is easy than you can imagine. And finally the subconscious is the mechanism through which thought impulses which are repeated regularly with feeling. And emotion are quickened. Duis turpis eros, tincidunt at, tempor a nisi. Fusce turpis mi, sollicitudin non volutpat id, ornare at nunc. Vestibulum tristique molestie mauris, non ultricies turpis consequat eget.</p>
+        <TeamSection/>
+    </div>
+  )
+}
+
+export default page
